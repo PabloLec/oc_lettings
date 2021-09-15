190d074a26950052b366766e3c90236767299275
PabloLec <pablo.lecolinet@gmail.com>
2021-09-15 15:19:10 +0200

    Add profiles integration tests

--- profiles/tests.py
+++ profiles/tests.py
@@ -1,4 +1,6 @@
 import pytest
+from django.urls import reverse
+from django.contrib.auth.models import User
 from profiles.models import Profile
 
 pytestmark = pytest.mark.django_db
@@ -21,3 +23,18 @@ class TestProfile:
         profile.delete()
         with pytest.raises(Profile.DoesNotExist):
             assert Profile.objects.get(favorite_city="Paris")
+
+
+def test_index(client):
+    response = str(client.get(reverse("profiles:index")).content)
+    assert "<title>Profiles</title>" in response
+    profiles = Profile.objects.all()
+    for el in profiles:
+        assert el.user.username in response
+
+
+def test_profile_page(client):
+    users = User.objects.all()
+    for el in users:
+        response = str(client.get(reverse("profiles:profile", args=(el.username,))).content)
+        assert el.username in response
