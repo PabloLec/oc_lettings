4bbc3ee9a2fc0f70f277f5cb0723121ecbb78a7e
PabloLec <pablo.lecolinet@gmail.com>
2021-09-20 13:01:13 +0200

    Set heroku config

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn --pythonpath oc_lettings oc_lettings_site.wsgi
--- oc_lettings/oc_lettings_site/settings.py
+++ oc_lettings/oc_lettings_site/settings.py
@@ -3,17 +3,16 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+if os.environ.get("DJANGO_ENV") == "DEV":
+    DEBUG = True
+    SECRET_KEY = "fp$9^593hsriajg$_%=5trot9g!1qa@ew(o-1#@=&4%=hp46(s"
+elif os.environ.get("DJANGO_ENV") == "PROD":
+    DEBUG = False
+    SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
+else:
+    raise EnvironmentError("Set DJANGO_ENV as either DEV or PROD.")
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "fp$9^593hsriajg$_%=5trot9g!1qa@ew(o-1#@=&4%=hp46(s"
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["oc-lettings-pablolec.heroku.app"]
 
 
 # Application definition
